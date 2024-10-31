2906be9ae7473d8f73eef10ae275b26b7f158661
Maksim <maksimkuzin73@yandex.ru>
2024-10-31 15:09:25 +0400

    recepients & warehouses

--- Controllers/RecepientController.cs
+++ Controllers/RecepientController.cs
@@ -1,12 +1,46 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using LogisticCompany.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace LogisticCompany.Controllers
 {
     public class RecepientController : Controller
     {
+        LCDBContext _db = new LCDBContext();
         public IActionResult Index()
         {
-            return View("RecepientsList");
+            var model = _db.Recepients.AsEnumerable();
+            return View("RecepientsList", model);
+        }
+        public IActionResult Create()
+        {
+            var model = new Recepient();
+            return View("CreateOrUpdate", model);
+        }
+        [HttpPost]
+        public IActionResult Create(Recepient recepient)
+        {
+            _db.Recepients.Add(recepient);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public IActionResult Edit(int id)
+        {
+            var model = _db.Recepients.FirstOrDefault(x => x.Id == id);
+            return View("CreateOrUpdate", model);
+        }
+        [HttpPost]
+        public IActionResult Edit(Recepient recepient)
+        {
+            _db.Recepients.Update(recepient);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public IActionResult Delete(int id)
+        {
+            var recepient = _db.Recepients.FirstOrDefault(x => x.Id == id);
+            _db.Remove(recepient);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
--- Controllers/WarehouseController.cs
+++ Controllers/WarehouseController.cs
@@ -1,12 +1,46 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using LogisticCompany.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace LogisticCompany.Controllers
 {
     public class WarehouseController : Controller
     {
+        LCDBContext _db = new LCDBContext();
         public IActionResult Index()
         {
-            return View("WarehousesList");
+            var model = _db.Warehouses.AsEnumerable();
+            return View("WarehousesList", model);
+        }
+        public IActionResult Create()
+        {
+            var model = new Warehouse();
+            return View("CreateOrUpdate", model);
+        }
+        [HttpPost]
+        public IActionResult Create(Warehouse warehouse)
+        {
+            _db.Warehouses.Add(warehouse);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public IActionResult Edit(int id)
+        {
+            var model = _db.Warehouses.FirstOrDefault(x => x.Id == id);
+            return View("CreateOrUpdate", model);
+        }
+        [HttpPost]
+        public IActionResult Edit(Warehouse warehouse)
+        {
+            _db.Warehouses.Update(warehouse);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public IActionResult Delete(int id)
+        {
+            var warehouse = _db.Warehouses.FirstOrDefault(x => x.Id == id);
+            _db.Warehouses.Remove(warehouse);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
